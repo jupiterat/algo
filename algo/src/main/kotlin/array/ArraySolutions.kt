23cc575e72782d0808144dc2ac98a4e42ea7f1ef
thoaidta <thoaidta@techvn.mti.co.jp>
2022-06-23 07:02:16 +0700

    Squares of a Sorted Array

--- algo/src/main/kotlin/array/ArraySolutions.kt
+++ algo/src/main/kotlin/array/ArraySolutions.kt
@@ -1,5 +1,6 @@
 package array
 
+import kotlin.math.abs
 import kotlin.math.max
 
 class ArraySolutions {
@@ -17,6 +18,37 @@ class ArraySolutions {
                     result = max(result, length)
                 } else {
                     length = 0
+                }
+            }
+            return result
+        }
+
+        fun findNumbers(nums: IntArray): Int {
+            var count = 0
+            for (i in nums) {
+                if (i.toString().length % 2 == 0) {
+                    count++
+                }
+            }
+            return count
+        }
+
+        /**
+         * Squares of a Sorted Array
+         */
+        fun sortedSquares(nums: IntArray): IntArray {
+            val size = nums.size
+            var left = 0
+            var right = size - 1
+            val result = IntArray(size)
+
+            for (i in size - 1 downTo 0) {
+                if (abs(nums[left]) > abs(nums[right])) {
+                    result[i] = nums[left] * nums[left]
+                    left++
+                } else {
+                    result[i] = nums[right] * nums[right]
+                    right--
                 }
             }
             return result
