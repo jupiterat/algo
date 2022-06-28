8f5191d489a44bd10bef23ac6dc35f180f3fe77a
thoaidta <thoaidta@techvn.mti.co.jp>
2022-06-28 20:58:50 +0700

    merge sorted arrays

--- algo/src/main/kotlin/array/ArraySolutions.kt
+++ algo/src/main/kotlin/array/ArraySolutions.kt
@@ -61,11 +61,11 @@ class ArraySolutions {
             var length = arr.size - 1
             println("length: ${length} ")
             for (left in 0 until length + 1) {
-                if(left > length - possible_dups) {
+                if (left > length - possible_dups) {
                     break
                 }
                 if (arr[left] == 0) {
-                    if(left == length - possible_dups) {
+                    if (left == length - possible_dups) {
                         arr[length] = 0
                         length -= 1
                         break
@@ -87,6 +87,25 @@ class ArraySolutions {
                 }
             }
             return arr
+        }
+
+        fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): IntArray {
+
+            var p1 = m - 1
+            var p2 = n - 1
+            var i = m + n - 1
+            while (p2 >= 0) {
+                if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                    nums1[i] = nums1[p1]
+                    p1--
+                } else {
+                    nums1[i] = nums2[p2]
+                    p2--
+                }
+                i--
+            }
+
+            return nums1
         }
     }
 
